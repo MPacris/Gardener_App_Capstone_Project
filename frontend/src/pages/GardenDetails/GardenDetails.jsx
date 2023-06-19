378ac0e951191c67214bddd1c106cb7da3dde44b
MPacris <mpacris@yahoo.com>
2023-06-19 11:58:11 -0500

    averagerating added to GardenDetails page

--- frontend/src/pages/GardenDetails/GardenDetails.jsx
+++ frontend/src/pages/GardenDetails/GardenDetails.jsx
@@ -42,13 +42,50 @@ const GardenDetails = () => {
 
     const fetchPlants = async () => {
       try {
-        const response = await axios.get("http://localhost:5000/api/plants", {
+        const plantResponse = await axios.get("http://localhost:5000/api/plants", {
           headers: {
             Authorization: "Bearer " + token,
           },
         });
+        
+        const plantData = plantResponse.data;
+
+        const harvestResponse = await axios.get("http://localhost:5000/api/harvests", {
+          headers: {
+            Authorization: "Bearer " + token,
+          },
+        });
+        
+        const harvestData = harvestResponse.data;
+
+        const plantMap = {};
+
+        harvestData.forEach((harvest) => {
+          if (!plantMap[harvest.plant_id]) {
+            plantMap[harvest.plant_id] = {
+              totalRating: 0,
+              count: 0,
+            };
+          }
+
+          plantMap[harvest.plant_id].totalRating += harvest.rating;
+          plantMap[harvest.plant_id].count++;
+        });
+
+        const updatedPlants = plantData.map((plant) => {
+          const averageRating =
+            plantMap[plant.id] && plantMap[plant.id].count
+              ? plantMap[plant.id].totalRating / plantMap[plant.id].count
+              : 0;
+
+          return {
+            ...plant,
+            average_harvest_rating: averageRating,
+          };
+        });
+
         if (mounted) {
-          setPlants(response.data);
+          setPlants(updatedPlants);
         }
       } catch (error) {
         // Handle error
@@ -188,7 +225,10 @@ const GardenDetails = () => {
       <div className="row">
         <div className="bottom-container">
           {filteredPlants.map((plant) => (
-            <div className="plant-card" key={plant.id}>
+            <div
+              className={`plant-card ${plant.average_harvest_rating <= 2 ? "red-background" : ""}`}
+              key={plant.id}
+            >
               <Link to={`/plant-details/${plant.id}`}>
                 <div>{plant.type}</div>
                 <div>{plant.location}</div>
@@ -199,6 +239,8 @@ const GardenDetails = () => {
                     className="plant-picture"
                   />
                 )}
+                <div>Garden ID: {plant.garden_id}</div>
+                <div>Average Harvest Rating: {plant.average_harvest_rating}</div>
               </Link>
             </div>
           ))}
