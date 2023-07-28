c7824d6fc3dfd44918519b5e05783c7185f067d1
MPacris <mpacris@yahoo.com>
2023-07-28 12:37:35 -0500

    added Logo to the Login Page to take up white space

---
+++ frontend/src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,59 @@
+import React, { useContext, useEffect } from "react";
+import AuthContext from "../../context/AuthContext";
+import useCustomForm from "../../hooks/useCustomForm";
+import { Link } from "react-router-dom";
+import "./LoginPage.css";
+
+const LoginPage = () => {
+  const { loginUser, isServerError } = useContext(AuthContext);
+  const defaultValues = { username: "", password: "" };
+  const [formData, handleInputChange, handleSubmit, reset] = useCustomForm(
+    defaultValues,
+    loginUser
+  );
+
+  useEffect(() => {
+    if (isServerError) {
+      reset();
+    }
+  }, [isServerError]);
+
+  return (
+    <div className="login-container">
+     
+      <div className="form-container">
+        <form className="form" onSubmit={handleSubmit}>
+          <label className="form-label">
+            Username:{" "}
+            <input
+              type="text"
+              name="username"
+              value={formData.username}
+              onChange={handleInputChange}
+            />
+          </label>
+          <label className="form-label">
+            Password:{" "}
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
+            />
+          </label>
+          {isServerError ? (
+            <p className="error">Login failed, incorrect credentials!</p>
+          ) : null}
+          <button className="submit-button">Login!</button>
+                <Link to="/register" className="form-link">
+          Click to register!
+        </Link>
+        </form>
+       
+      </div>
+      <img src="/uploadpictures/GreenThumbLogo.png" alt="Background" className="background-img" />
+    </div>
+  );
+};
+
+export default LoginPage;
